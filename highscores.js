8f63ebd67d5dade6105763e60e340b702b232d93
ritwickghoshcse-2020 <122892342+ritwickghoshcse-2020@users.noreply.github.com>
2024-05-17 15:12:24 +0530

    Add files via upload

---
+++ highscores.js
@@ -0,0 +1,10 @@
+const highscoresList = document.getElementById("highScoreList");
+const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+highscoresList.innerHTML = highscores
+.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+
+})
+
+.join("");
